8a9b5f00217f8fd6af30ffd3e81575ae0a67f570
Morteza Bashsiz <morteza.bashsiz@gmail.com>
2023-10-31 15:07:53 +0100

    add help to find

--- src/find/main.cpp
+++ src/find/main.cpp
@@ -4,27 +4,57 @@
 using namespace cv;
 using namespace std;
 
+static void help(char** argv)
+{
+    cout << "Usage:\n"
+        <<  argv[0]
+        <<  "   [--cascade=<cascade_path> this is the primary trained classifier such as frontal face]\n"
+        		"		[--width]\n"
+        		"		[--height]\n"
+            "   [--camera]\n\n"
+            "example:\n"
+        <<  argv[0]
+        <<  " --cascade=\"./haarcascade_frontalface_alt.xml\" --width=800 --height=600 --camera=0 \n";
+}
+
 int main(int argc, char** argv)
 {
-	Mat myImage;
+	
+	cv::CommandLineParser parser(argc, argv,
+			"{help h||}"
+			"{cascade|data/haarcascades/haarcascade_frontalface_alt.xml|}"
+			"{width|800|}"
+			"{height|600|}"
+			"{camera|0|}"
+	);
+	if (parser.has("help"))
+	{
+			help(argv);
+			return 0;
+	}
+	int width = parser.get<int>("width");
+	int height = parser.get<int>("height");
+	int camera = parser.get<int>("camera");
+	Mat Frame;
 	namedWindow("Video Player");
-	VideoCapture cap(0);
-	cap.set(CAP_PROP_FRAME_WIDTH,800);
-	cap.set(CAP_PROP_FRAME_HEIGHT,600);
+	VideoCapture cap(camera);
+	cap.set(CAP_PROP_FRAME_WIDTH,	width);
+	cap.set(CAP_PROP_FRAME_HEIGHT,	height);
 	if (!cap.isOpened()){
 		cout << "No video stream detected" << endl;
 		system("pause");
 		return-1;
 	}
 	while (true){
-		cap >> myImage;
-		if (myImage.empty()){
+		cap >> Frame;
+		if (Frame.empty()){
 			break;
 		}
 		vector<vector<Point> > squares;
-		findSquares(myImage, squares);
-		polylines(myImage, squares, true, Scalar(0, 255, 0), 3, LINE_AA);
-		imshow("Video Player", myImage);
+		findSquares(Frame, squares);
+		// detectAndDraw(Frame, cascade, nestedCascade, scale, tryflip );
+		polylines(Frame, squares, true, Scalar(0, 255, 0), 3, LINE_AA);
+		imshow("Video Player", Frame);
 		char c = (char)waitKey(25);
 		if (c == 27){
 			break;
